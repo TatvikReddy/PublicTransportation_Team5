a9f8ec2d5a3e2563411efde0e683d4ef713e0ae2
Eric Zhang <ericzhang631@gmail.com>
2024-03-24 19:14:39 -0500

    handle duplicate usernames

--- TicketSwift/server/connect.cjs
+++ TicketSwift/server/connect.cjs
@@ -6,14 +6,32 @@ async function main()
     const Db = process.env.ATLAS_URI
     const client = new MongoClient(Db)
 
+    const username = "bob"
+    const email_address = "bob69@fakegmail.com"
+    const password = "assdffewfaef"
+
     try
     {
         await client.connect()
         const comments = await client.db("sample_mflix").collection("comments")
         
-        const query = {name: "poop"}
+        const query = {name: username}
 
         const result = await comments.findOne(query)
+        if(result != null)
+        {
+            console.log("Username already exists")
+        }
+        else
+        {
+            const doc = {
+                name: username,
+                email: email_address,
+                text: password,
+            }
+            const res = await comments.insertOne(doc)
+            console.log("Account successfully created")
+        }
 
         console.log(result)
     }
