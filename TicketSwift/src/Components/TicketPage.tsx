8bd101b31d760531d8e1d82c6f9f85ddb61344e7
TatvikReddy <tatvikmum@gmail.com>
2024-04-19 03:17:03 -0500

    Fix styling issues, added a back to profile button and updated Ticket page to read output

--- TicketSwift/src/Components/TicketPage.tsx
+++ TicketSwift/src/Components/TicketPage.tsx
@@ -1,60 +1,92 @@
 import { Link } from 'react-router-dom';
 import '../App.css';
 import TicketSwiftLogo from '../TicketSwiftLogo.png';
+import React, { useState } from 'react';
 
 function TicketPage() {
   // This page will show the ticket information with a QR code, similar to the one shown in the image
+  const [passengerName, setPassengerName] = useState('');
+  const [fromLocation, setFromLocation] = useState('');
+  const [toLocation, setToLocation] = useState('');
+  const [seatNumber, setSeatNumber] = useState('');
+  const [classStatus, setClassStatus] = useState(''); 
+  const [companyName, setCompanyName] = useState('');
+  const [departTime, setDepartTime] = useState('');
+  const [arriveTime, setArriveTime] = useState('');
+  const [status, setStatus] = useState('');
+  const [price, setPrice] = useState('');
+
   return (
     
       
-    <div style={{ marginTop: '100px'}}>
+    <div style={{marginTop: '100px'}}>
       <h1>Ticket Information</h1>
-      <div className="ticket-info" >
-      <div className="ticket-route" style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '65px' }}>
-          <span className="ticket-passenger" style={{ fontSize: '14px' }}>Passenger</span>
-          <span className="ticket-passenger-name" style={{ fontSize: '18px' }}>John Doe</span>
+      <div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
+      <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+        <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Passenger</span>
+        <span >
+          {passengerName || 'John Doe'}
+        </span>
+      </div>
+      <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+        <span style={{ fontSize: '20px', fontWeight: 'bold' }}>From</span>
+        <span >
+          {fromLocation || 'Dallas'}
+        </span>
+      </div>
+      <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+        <span style={{ fontSize: '20px', fontWeight: 'bold' }}>To</span>
+        <span >
+          {toLocation || 'Houston'}
+        </span>
+      </div>
+      </div>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Seat</span>
+          <span >
+            {seatNumber || 'A1'}
+          </span>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '50px' }}>
-          <span className="ticket-from" style={{ fontSize: '14px' }}>From</span>
-          <span className="ticket-from-location" style={{ fontSize: '18px' }}>Dallas</span>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Class</span>
+          <span >
+            {classStatus || 'First Class'}
+          </span>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '30px' }}>
-          <span className="ticket-to" style={{ fontSize: '14px' }}>To</span>
-          <span className="ticket-to-location" style={{ fontSize: '18px' }}>Houston</span>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Company</span>
+          <span >
+            {companyName || 'TicketSwift'}
+          </span>
         </div>
       </div>
-      <div className="ticket-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '60px' }}>
-          <span className="ticket-seat" style={{ fontSize: '14px' }}>Seat</span>
-          <span className="ticket-seat-num" style={{ fontSize: '18px' }}>12A</span>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Depart</span>
+          <span >
+            {departTime || '12:00 PM'}
+          </span>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 35px' }}>
-          <span className="ticket-class" style={{ fontSize: '14px' }}>Class</span>
-          <span className="ticket-class-status" style={{ fontSize: '18px' }}>Economy</span>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Arrive</span>
+          <span >
+            {arriveTime || '2:00 PM'}
+          </span>
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '45px' }}>
-          <span className="ticket-company" style={{ fontSize: '14px' }}>Company</span>
-          <span className="ticket-company-name" style={{ fontSize: '18px' }}>XYZ</span>
-        </div>
-      </div>
-      <div className="ticket-details" style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginRight: '30px' }}>
-          <span className="ticket-depart" style={{ fontSize: '14px' }}>Depart</span>
-          <span className="ticket-depart-time" style={{ fontSize: '18px' }}>10:00 AM</span>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '0 45px' }}>
-          <span className="ticket-arrive" style={{ fontSize: '14px' }}>Arrive</span>
-          <span className="ticket-arrive-time" style={{ fontSize: '18px' }}> 2:00 PM</span>
-        </div>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginLeft: '40px' }}>
-          <span className="ticket-status" style={{ fontSize: '14px' }}>Status</span>
-          <span className="ticket-status-time" style={{ fontSize: '18px' }}>On Time</span>
+        <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+          <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Status</span>
+          <span >
+            {status || 'On Time'}
+          </span>
         </div>
       </div>
-      <div className="ticket-price" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <span style={{ fontSize: '14px' }}>Price</span>
-        <span style={{ fontSize: '18px' }}>$200</span>
+      <div style={{ flex: '1 0 30%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '10px' }}>
+        <span style={{ fontSize: '20px', fontWeight: 'bold' }}>Price</span>
+        <span >
+          {price || '$50.00'}
+        </span>
       </div>
       </div>
       <div style={{ marginTop: "20px", display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
